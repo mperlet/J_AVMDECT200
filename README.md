2eb3c53ae348bc19d38baebd45a11b1d1cd3ff4a
Mathias Perlet <mathias.perlet@pipesbox.de>
2015-03-20 09:13:14 +0100

    Added documentation

--- README.md
+++ README.md
@@ -28,3 +28,127 @@ Watt: 	4.0W
 State: 	ON
 Temp: 	22.8°C
 ```
+
+# Documentation
+
+## `public class AVMDect200`
+
+AVM Fritzbox Dect-200 Socket Api-Wrapper
+
+ * **Author:** Mathias Perlet
+ * **Version:** 0.0.1
+
+## `public AVMDect200(String fritzbox_pw)`
+
+ * **Parameters:** `fritzbox_pw` — the frizbox password for login
+
+## `public AVMDect200(String fritzbox_pw, String host)`
+
+ * **Parameters:**
+   * `fritzbox_pw` — the frizbox password for login
+   * `host` — the fritzbox url, standard is http://fritz.box
+
+## `public void init() throws IOException, InvalidPasswordException`
+
+initial communication with the fritzbox generates a session id for further actions
+
+ * **Exceptions:**
+   * `IOException` — connections errors, like host not found
+   * `InvalidPasswordException` — throws if password not valid
+
+## `private String homeauto_url_with_sid()`
+
+ * **Returns:** the absolute url to fritzbox-api with valid session key
+
+## `private String callCommand(String command, String devId) throws IOException`
+
+ * **Parameters:**
+   * `command` — a valid command from dect200 documentation
+   * `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** api-response from fritzbox
+ * **Exceptions:** `IOException` — communication errors
+
+## `public String getInfo() throws IOException`
+
+ * **Returns:** returns a comma separated list of connected socket IDs
+ * **Exceptions:** `IOException` — 
+
+## `public String getSocketName(String devId) throws IOException`
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** the configured socket-name like "Living Room"
+ * **Exceptions:** `IOException` — 
+
+## `public Boolean getSocketAvailability(String devId) throws IOException`
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** true if the socket is available
+ * **Exceptions:** `IOException` — 
+
+## `public int getSocketPower(String devId)`
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** the current power in milli watt as a integer, on error it returns the minimal integer value
+ * **Exceptions:** `IOException` — 
+
+## `public int getSocketTemperature(String devId) throws IOException`
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:**
+   * the current temperature with offset in centi grad celsius (245 is 24.5°C).
+   * on error, it returns integer min value
+ * **Exceptions:** `IOException` — 
+
+## `public String setSocketOn(String devId) throws IOException`
+
+switch a socket on
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** the success value for switch the socket on
+ * **Exceptions:** `IOException` — 
+
+## `public String setSocketOff(String devId) throws IOException`
+
+switch a socket off
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** the success value for switch the socket off
+ * **Exceptions:** `IOException` — 
+
+## `public boolean getSocketState(String devId) throws IOException`
+
+ * **Parameters:** `devId` — the dect200 socket AIN (socket-id)
+ * **Returns:** true if socket is on, false if socket if off
+ * **Exceptions:** `IOException` — 
+
+## `public ArrayList<Socket> getSocketList() throws IOException`
+
+ * **Returns:** a ArryList of sockets
+ * **Exceptions:** `IOException` — 
+
+## `public String getSID()`
+
+ * **Returns:** current session id for fritzbox communication
+
+## `private String readURL(String url) throws IOException`
+
+ * **Parameters:** `url` — http communication with the fritzbox-api, only http-GET
+ * **Returns:** returns the http response
+ * **Exceptions:** `IOException` — 
+
+## `private String getTagValue(String xml, String tagName)`
+
+ * **Parameters:**
+   * `xml` — every tag must be unique, otherwise it will return an empty string
+   * `tagName` — name of the xml-tag
+ * **Returns:** returns the value from the tag as a string
+
+## `private String md5(String md5)`
+
+ * **Parameters:** `md5` — string to hash
+ * **Returns:** returns the md5-hash in utf-16le encoding
+
+## `public static JSONObject xmlToJson(String callbackResponse)`
+
+ * **Parameters:** `callbackResponse` — xml string
+ * **Returns:** a json-object
--- src/AVMDECT200/AVMDect200.java
+++ src/AVMDECT200/AVMDect200.java
@@ -1,3 +1,4 @@
+package org.ws4d.pipes.packages.fritz.server.api;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,90 +13,169 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
 
+/**
+ * AVM Fritzbox Dect-200 Socket Api-Wrapper
+ * 
+ * @author Mathias Perlet
+ * @version 0.0.1
+ *
+ */
 public class AVMDect200 {
+	// standard fritzbox url
 	private String fritzbox_url = "fritz.box";
+	// initial fritzbox password
 	private String fritzbox_pw = "";
+	// initial session id
 	private String SID = "0000000000000";
+	//path to fritz api
 	private String homeswitch = "/webservices/homeautoswitch.lua";
 
+	/**
+	 * @param fritzbox_pw the frizbox password for login
+	 */
 	public AVMDect200(String fritzbox_pw) {
 		this.fritzbox_pw = fritzbox_pw;
 	}
-
+	
+	
+	/**
+	 * @param fritzbox_pw the frizbox password for login
+	 * @param host the fritzbox url, standard is http://fritz.box
+	 */
 	public AVMDect200(String fritzbox_pw, String host) {
 		this.fritzbox_pw = fritzbox_pw;
 		this.fritzbox_url = host;
 	}
 
+	/**
+	 * initial communication with the fritzbox
+	 * generates a session id for further actions
+	 * 
+	 * @throws IOException connections errors, like host not found
+	 * @throws InvalidPasswordException throws if password not valid
+	 */
 	public void init() throws IOException, InvalidPasswordException {
 		if (!fritzbox_url.startsWith("http://"))
 			this.fritzbox_url = "http://" + this.fritzbox_url;
 		String base_url = fritzbox_url + "/login_sid.lua";
 		String challenge = getTagValue(readURL(base_url), "Challenge");
-		String response = challenge + "-" + MD5(challenge + "-" + fritzbox_pw);
+		String response = challenge + "-" + md5(challenge + "-" + fritzbox_pw);
 		SID = getTagValue(readURL(base_url + "?response=" + response), "SID");
 		if (SID.equals("0000000000000000"))
 			throw new InvalidPasswordException("invalid password");
 	}
 
+	/**
+	 * @return the absolute url to fritzbox-api with valid session key
+	 */
 	private String homeauto_url_with_sid() {
 		return this.fritzbox_url + this.homeswitch + "?sid=" + this.SID;
 	}
 
+	/**
+	 * @param command a valid command from dect200 documentation
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return api-response from fritzbox
+	 * @throws IOException communication errors
+	 */
 	private String callCommand(String command, String devId) throws IOException {
 		String url = homeauto_url_with_sid() + "&switchcmd=" + command
 				+ "&ain=" + devId;
 		return readURL(url);
 	}
 
+	/**
+	 * @return returns a comma separated list of connected socket IDs
+	 * @throws IOException
+	 */
 	public String getInfo() throws IOException {
 		String url = homeauto_url_with_sid() + "&switchcmd=getswitchlist";
 		return readURL(url);
 	}
 
+	/**
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return the configured socket-name like "Living Room" 
+	 * @throws IOException
+	 */
 	public String getSocketName(String devId) throws IOException {
 		return callCommand("getswitchname", devId);
 	}
-	
+
+	/**
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return true if the socket is available
+	 * @throws IOException
+	 */
 	public Boolean getSocketAvailability(String devId) throws IOException {
 		return !callCommand("getswitchpower", devId).equals("inval");
 	}
-	
-	public int getSocketPower(String devId) throws IOException {
-		
+
+	/**
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return the current power in milli watt as a integer, on error it returns the minimal integer value
+	 * @throws IOException
+	 */
+	public int getSocketPower(String devId) {
+
 		try {
 			return Integer.parseInt(callCommand("getswitchpower", devId));
 		} catch (NumberFormatException | IOException e) {
 			return Integer.MIN_VALUE;
 		}
 	}
-	
+
+	/**
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return the current temperature with offset in centi grad celsius (245 is 24.5°C).
+	 * @return on error, it returns integer min value
+	 * @throws IOException
+	 */
 	public int getSocketTemperature(String devId) throws IOException {
 		try {
 			JSONArray sockets = xmlToJson(
-								callCommand("getdevicelistinfos", devId))
-								.getJSONObject("devicelist").
-								getJSONArray("device");
+					callCommand("getdevicelistinfos", devId))
+					.getJSONObject("devicelist").
+					getJSONArray("device");
 
-		for (int i = 0; i < sockets.length(); i++) {
-			JSONObject s = sockets.getJSONObject(i);
-			if(s.getString("identifier").replace(" " , "").equals(devId))
-				return s.getJSONObject("temperature").getInt("celsius") + 
-						s.getJSONObject("temperature").getInt("offset");
-		}
+			for (int i = 0; i < sockets.length(); i++) {
+				JSONObject s = sockets.getJSONObject(i);
+				if(s.getString("identifier").replace(" " , "").equals(devId))
+					return s.getJSONObject("temperature").getInt("celsius") + 
+							s.getJSONObject("temperature").getInt("offset");
+			}
 		} catch (JSONException e) {
 		}
 		return Integer.MIN_VALUE;
 	}
 
+	/**
+	 * switch a socket on
+	 * 
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return the success value for switch the socket on
+	 * @throws IOException
+	 */
 	public String setSocketOn(String devId) throws IOException {
 		return callCommand("setswitchon", devId);
 	}
 
+	/**
+	 * switch a socket off
+	 * 
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return the success value for switch the socket off
+	 * @throws IOException
+	 */
 	public String setSocketOff(String devId) throws IOException {
 		return callCommand("setswitchoff", devId);
 	}
 
+	/**
+	 * @param devId the dect200 socket AIN (socket-id)
+	 * @return true if socket is on, false if socket if off
+	 * @throws IOException
+	 */
 	public boolean getSocketState(String devId) throws IOException {
 		String ret = callCommand("getswitchstate", devId);
 		if (ret.equals("1"))
@@ -103,6 +183,10 @@ public class AVMDect200 {
 		return false;
 	}
 
+	/**
+	 * @return a ArryList of sockets
+	 * @throws IOException
+	 */
 	public ArrayList<Socket> getSocketList() throws IOException {
 		ArrayList<Socket> Sockets = new ArrayList<Socket>();
 		String[] devs = getInfo().split(",");
@@ -112,11 +196,19 @@ public class AVMDect200 {
 		return Sockets;
 	}
 
+	/**
+	 * @return current session id for fritzbox communication
+	 */
 	public String getSID() {
 		return this.SID;
 	}
 
-	public String readURL(String url) throws IOException {
+	/**
+	 * @param url http communication with the fritzbox-api, only http-GET
+	 * @return returns the http response
+	 * @throws IOException
+	 */
+	private String readURL(String url) throws IOException {
 		URL temp_url = new URL(url);
 		URLConnection yc = temp_url.openConnection();
 		// set 10 seconds timeout
@@ -126,7 +218,12 @@ public class AVMDect200 {
 		return in.readLine();
 	}
 
-	public String getTagValue(String xml, String tagName) {
+	/**
+	 * @param xml every tag must be unique, otherwise it will return an empty string
+	 * @param tagName name of the xml-tag
+	 * @return returns the value from the tag as a string
+	 */
+	private String getTagValue(String xml, String tagName) {
 		try {
 			return xml.split("<" + tagName + ">")[1]
 					.split("</" + tagName + ">")[0];
@@ -135,7 +232,11 @@ public class AVMDect200 {
 		}
 	}
 
-	public String MD5(String md5) {
+	/**
+	 * @param md5 string to hash
+	 * @return returns the md5-hash in utf-16le encoding
+	 */
+	private String md5(String md5) {
 		try {
 			java.security.MessageDigest md = java.security.MessageDigest
 					.getInstance("MD5");
@@ -156,7 +257,11 @@ public class AVMDect200 {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * @param callbackResponse xml string
+	 * @return a json-object
+	 */
 	public static JSONObject xmlToJson(String callbackResponse) {
 		JSONObject xmlJSONObj = new JSONObject();
 		try {
@@ -171,7 +276,7 @@ public class AVMDect200 {
 		}
 		return xmlJSONObj;
 	}
-	
+
 	public class Socket {
 		public String name;
 		public String id;
@@ -191,7 +296,7 @@ public class AVMDect200 {
 			return this.name;
 		}
 
-		public int getSocketPower() throws IOException {
+		public int getSocketPower() {
 			return jF.getSocketPower(this.id);
 		}
 
@@ -214,15 +319,15 @@ public class AVMDect200 {
 		public boolean getSocketState() throws IOException {
 			return jF.getSocketState(this.id);
 		}
-		
+
 		public Boolean getSocketAvailability() throws IOException {
 			return jF.getSocketAvailability(this.id);
 		}
-		
-		public int getSocketTemperature() throws IOException, JSONException {
+
+		public int getSocketTemperature() throws IOException {
 			return jF.getSocketTemperature(this.id);
 		}
-		
+
 		public String toString() {
 			return this.id + ":" + this.name;
 		}
